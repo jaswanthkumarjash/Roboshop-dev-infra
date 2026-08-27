bb2bdf7af38eade1220fb00d208c486a4b172e07
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2026-08-27 10:13:44 +0530

    Terraform practice

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -4,4 +4,8 @@ module "vpc" {
     project = var.project_name
     environment = var.environment
     vpc_tags = var.vpc_tags
+
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
 }
