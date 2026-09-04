e04c6ffc001adc265f92c631569bbd59101cce11
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2026-09-04 12:56:42 +0530

    Terraform practice

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z016704122JSXT2DSCQ40"
+}
+
+variable "domain_name" {
+    default = "devopspractice.site"
+}
